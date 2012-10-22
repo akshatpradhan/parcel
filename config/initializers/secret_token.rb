e31389c9bfdb06511dfbd669e1017d909b8ad33f
Akshat Pradhan <akshat.ip@gmail.com>
2012-10-22 00:57:00 -0400

    add rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parcel::Application.config.secret_token = 'ebea5d2d69016e7d72b76b3a909fd06e4f4ed5b883e4b53b616f14539c8ef6bfa926903bdbb8d81a9858c6f389695591ac2ed48afba6c69213a6578be23a0e1f'
