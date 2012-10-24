bef7751fc9f5bc1a5adc65007e35da8e43f6e22b
Akshat Pradhan <akshat.ip@gmail.com>
2012-10-24 03:41:50 -0400

    add google maps and add CRUD to Garden

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,5 +1,13 @@
 class Garden
   include Mongoid::Document
-  field :title, type: String
+  include Mongoid::Timestamps
+  include Gmaps4rails::ActsAsGmappable
+
+  acts_as_gmappable :address => :address, :position => :position
+
+  field :title,       type: String
   field :description, type: String
+  field :address,     type: String
+  field :position,    type: Array
+  field :gmaps,       type: Boolean
 end
