00775ce3f1a5d506d9d402f577e1d9033651f595
Akshat Pradhan <akshat.ip@gmail.com>
2012-10-24 16:45:01 -0400

    improve route so that root_url redirects to /gardens

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ Parcel::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'gardens#index'
 
   # See how all your routes lay out with "rake routes"
 
